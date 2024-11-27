31ae015f616f5020164bb374dbac2504c5ef33b5
brentlier <brent.depoortere@student.kuleuven.be>
2024-11-27 23:00:09 +0100

    Button should reload the gun and catch the enemy when you're near

--- Assets/BycicleSystem/BicycleVehicle.cs
+++ Assets/BycicleSystem/BicycleVehicle.cs
@@ -15,6 +15,8 @@ public class BicycleVehicle : MonoBehaviour
     private bool isSerialRunning = false;
     public int buttonPressed = 0;
 
+    public EnemyController enemy;
+
     private string lastReceivedData = "";
     private float lastFireTime = -5f;
 
@@ -81,8 +83,6 @@ public class BicycleVehicle : MonoBehaviour
     public Vector3 cameraOffset = new Vector3(0f, 2f, -5f); // Offset from the bike
     public float smoothFollowSpeed = 0.1f; // Speed of smooth following
     private Vector3 smoothDampVelocity; // For smooth damp calculations
-
-
 
     void Start()
     {
@@ -207,7 +207,10 @@ public class BicycleVehicle : MonoBehaviour
             {
                 buttonPressed = parsedButton;
                 if(buttonPressed == 1){
-                    //UnityEngine.Debug.Log("ButtonPressed");
+                    gun.ReloadBullets();
+                    if(enemy.NearPlayer = true){
+                        enemy.TriggerCutscene();
+                    }
                 }
             }
             else
@@ -419,6 +422,5 @@ public class BicycleVehicle : MonoBehaviour
             serialPort.Close();
         }
     }
-
 
 }
--- Assets/scripts/bullet.cs
+++ Assets/scripts/bullet.cs
@@ -46,4 +46,6 @@ public class Bullet : MonoBehaviour
             Destroy(explosion, 2f); 
         }
     }
+
+    
 }
