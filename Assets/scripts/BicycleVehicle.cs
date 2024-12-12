3468c5a27ce47e3aa753e9a05a03b2667f7f8246
Niels Coopmans <nielscoopmans@gmail.com>
2024-12-12 12:15:29 +0100

    arrow gone in garage

--- Assets/scripts/BicycleVehicle.cs
+++ Assets/scripts/BicycleVehicle.cs
@@ -30,6 +30,7 @@ public class BicycleVehicle : MonoBehaviour
     public TextMeshProUGUI InfoButton; 
     public TextMeshProUGUI TimeLeft;
     public TextMeshProUGUI NearInfo;
+    public GameObject arrow;
 
     private bool arduinoData;
 
@@ -341,6 +342,7 @@ public class BicycleVehicle : MonoBehaviour
                         InfoButton.enabled = false;
                         InfoGun.enabled = false;
                         TimeLeft.enabled = true;
+                        arrow.SetActive(true);
                     }
                 }
                 else
